cb33c5e4fc3a378d8f2aa30afc4f3fd7bfa464df
Mathieu de Maleingreau <mdemaleingreau@gmail.com>
2019-05-12 21:57:12 +0200

    recherche par album via spotify

--- VinylStore/Controllers/DiscogsController.cs
+++ VinylStore/Controllers/DiscogsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpotifyWebAPI;
 using VinylStore.Models;
 
 namespace VinylStore.Controllers
@@ -41,8 +42,39 @@ namespace VinylStore.Controllers
             ////on l'utilise pour récupérer les données à enregistrer (dont la tracklist!!)
             //string urlWithReleaseId
 
-            var output = await SpotifyWebAPI.Artist.GetArtist(query);
-            return View("SearchResult", output);
+            string queryString = "https://api.spotify.com/v1/search?q=album:" + query.Replace(" ", "%20");
+
+            //if (artistName != "")
+            //    queryString += "%20:artist:" + artistName.Replace(" ", "%20");
+            //if (year != "")
+            //    queryString += "%20:year:" + year.Replace(" ", "%20");
+            //if (genre != "")
+            //    queryString += "%20:genre:" + genre.Replace(" ", "%20");
+            //if (upc != "")
+            //    queryString += "%20:upc:" + upc.Replace(" ", "%20");
+            //if (isrc != "")
+            //    queryString += "%20:isrc:" + isrc.Replace(" ", "%20");
+
+            //queryString += "&limit=" + limit;
+            //queryString += "&offset=" + offset;
+            queryString += "&type=album";
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Headers.Add("Authorization", "Bearer BQCT0A6vl8bjfRH13hugiaYdUaQeVw4ksPqZKwPaQwLaBbV9JA7J31PvcCpnvgLOzOalCZv4W-vi3Hbyx7A");
+                 
+                request.Method = HttpMethod.Get;
+                request.RequestUri = new Uri(queryString);
+                var result = await client.SendAsync(request);
+                //tester statuscode
+                var parsedResult = await result.Content.ReadAsAsync<SearchVinyl2>();
+                return View("SearchResult", parsedResult);
+                
+            }
+
+            //var output = await SpotifyWebAPI.Artist.GetArtist(queryString);
+            //return View("SearchResult", output);
         }
     }
 }
